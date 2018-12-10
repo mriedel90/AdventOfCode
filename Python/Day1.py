fb784ec46820d1f3af4f1d86709478af0f688bcc
Matt Riedel <dev6@sovren.com>
2018-12-09 23:37:04 -0500

    day 1 part 2, takes like 5 mins to run...

--- Python/Day1.py
+++ Python/Day1.py
@@ -1,8 +1,52 @@
 
-fequency = 0
+def calculateFrequency(frequecyChanges):
+    frequency = 0
+    for change in frequencyChanges:
+        frequency += change
+    return frequency
+
+def calculateFirstFrequencyTwice(frequencyChanges):
+    frequency = 0
+    frequencies = [0]
+    while True:
+        for change in frequencyChanges:
+            frequency += change
+            if frequency in frequencies:
+                return frequency
+            frequencies.append(frequency)
+
+
+
+frequencyChanges = [1, -1]
+firstFrequencyTwice = calculateFirstFrequencyTwice(frequencyChanges)
+print('first frequency twice is: ' + str(firstFrequencyTwice) + ' should be 0')
+
+
+frequencyChanges = [3, 3, 4, -2, -4]
+firstFrequencyTwice = calculateFirstFrequencyTwice(frequencyChanges)
+print('first frequency twice is: ' + str(firstFrequencyTwice) + ' should be 10')
+
+
+frequencyChanges = [-6, 3, 8, 5, -6]
+firstFrequencyTwice = calculateFirstFrequencyTwice(frequencyChanges)
+print('first frequency twice is: ' + str(firstFrequencyTwice) + ' should be 5')
+
+
+frequencyChanges = [7, 7, -2, -7, -4]
+firstFrequencyTwice = calculateFirstFrequencyTwice(frequencyChanges)
+print('first frequency twice is: ' + str(firstFrequencyTwice) + ' should be 14')
+
+
+
+# read from file
+frequencyChanges = []
 inputFile = open("Day1.txt", "r")
-for nextFrequency in inputFile.readlines():
-    fequency += int(nextFrequency)
+for line in inputFile.readlines():
+    frequencyChanges.append(int(line))
 inputFile.close()
 
-print('frequency is: ' + str(fequency))
+freq = calculateFrequency(frequencyChanges)
+print('frequency is: ' + str(freq))
+
+firstFrequencyTwice = calculateFirstFrequencyTwice(frequencyChanges)
+print('first frequency twice is: ' + str(firstFrequencyTwice))
