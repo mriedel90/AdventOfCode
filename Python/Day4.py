0530faf8f59ebd62c3f6efcba6677d60c1acb23f
Matt Riedel <dev6@sovren.com>
2018-12-16 21:55:16 -0500

    day 4 part 2

--- Python/Day4.py
+++ Python/Day4.py
@@ -1,6 +1,6 @@
 from helperMethods import readFile
 
-def findGuardTimesMinute(inputs):
+def findGuardTimesMinuteStrategy1(inputs):
     inputs.sort()
     guards = dict()
     currentGuard = ''
@@ -31,10 +31,51 @@ def findGuardTimesMinute(inputs):
 
     return guardWithMaxLength * mostCommonMinute
 
+def findGuardTimesMinuteStrategy2(inputs):
+    inputs.sort()
+    guards = dict()
+    currentGuard = ''
+    guardAsleepTime = -1
+    guardAwakeTime = -1
+    for input in inputs:
+        if input.__contains__('begins shift'):
+            currentGuard = int(input.split('Guard #')[1].split(' ')[0])
+            if currentGuard not in guards:
+                guards[currentGuard] = []
+        if input.__contains__('falls asleep'):
+            guardAsleepTime = int(input.split(':')[1].split(']')[0])
+        if input.__contains__('wakes up'):
+            guardAwakeTime = int(input.split(':')[1].split(']')[0])
+        if guardAsleepTime >= 0 and guardAwakeTime >= 0:
+            for minute in range(guardAsleepTime, guardAwakeTime):
+                guards[currentGuard].append(minute)
+            guardAsleepTime = -1
+            guardAwakeTime = -1
+    
+    maxOccurrancesOfMinute = 0
+    maxMinute = -1
+
+    for guard in guards:
+        minutes = guards[guard]
+        if len(minutes) == 0:
+            continue
+        mostCommonMinute = max(set(minutes), key=minutes.count)
+        occurs = minutes.count(mostCommonMinute)
+        if occurs > maxOccurrancesOfMinute:
+            guardWithMaxLength = guard
+            maxOccurrancesOfMinute = occurs
+            maxMinute = mostCommonMinute
+
+    return guardWithMaxLength * maxMinute
+
 inputs = readFile("Day4Sample.txt")
-output = findGuardTimesMinute(inputs)
+output = findGuardTimesMinuteStrategy1(inputs)
 print(output) #240
+output = findGuardTimesMinuteStrategy2(inputs)
+print(output) #4455
 
 inputs = readFile("Day4.txt")
-output = findGuardTimesMinute(inputs)
-print(output)
+output = findGuardTimesMinuteStrategy1(inputs)
+print(output) #118599
+output = findGuardTimesMinuteStrategy2(inputs)
+print(output) #
