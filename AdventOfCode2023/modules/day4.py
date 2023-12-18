29fddce0740d450cd894d3f04f3f92a48b2f8b62
Riedel, Matthew <Matthew_Riedel@comcast.com>
2023-12-18 14:48:23 -0500

    day 4 part 2

--- AdventOfCode2023/modules/day4.py
+++ AdventOfCode2023/modules/day4.py
@@ -13,7 +13,9 @@ def ParseCard(line):
     winningNumbers = numberSets[0]
     cardNumbers = numberSets[1]
     points = CalcPoints(winningNumbers, cardNumbers)
-    return {'id': id, 'winningNumbers': winningNumbers, 'cardNumbers': cardNumbers, 'points': points}
+    cntMatchingNumbers = CalcMatchingNumbers(winningNumbers, cardNumbers)
+
+    return {'id': id, 'winningNumbers': winningNumbers, 'cardNumbers': cardNumbers, 'points': points, 'cntMatchingNumbers': cntMatchingNumbers}
 
 def CalcPoints(winningNumbers, cardNumbers):
     points = 0
@@ -44,17 +46,40 @@ def SumPoints(file):
 def CalcNumberOfTotalCards(file):
     lines = readFileAsLines(file)
     cards = [ParseCard(line) for line in lines]
+    
+    # start with 1 of each card
+    cntEachCard = [1] * len(cards)
+
+    for card in cards:
+        for i in range(0, card['cntMatchingNumbers']):
+            cntEachCard[card['id'] + i] += cntEachCard[card['id'] - 1]
+
+    totalCards = sum(cntEachCard)
+    return totalCards
 
 
 
 
-file = 'inputs/day4.txt'
+
+
+file = 'inputs/day4-sample.txt'
 sumPoints = SumPoints(file)
+numCards = CalcNumberOfTotalCards(file)
 
 print('Sum of Points: ' + str(sumPoints))
+print('Num Cards: ' + str(numCards))
 
 
 
 # part 1
 # sample: Sum of Points: 13
 # input: Sum of Points: 23235
+
+#part 2 - sample:
+# Sum of Points: 13
+# Num Cards: 30
+
+#part 2 - input:
+
+# Sum of Points: 23235
+# Num Cards: 5920640
